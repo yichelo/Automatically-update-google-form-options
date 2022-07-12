70fa70a5674a271d452e2d10851f2dcc0f67e9ab
yichelo <j30634t@yahoo.com.tw>
2022-07-12 15:40:21 +0800

    Create responsesToSheet.js

---
+++ responsesToSheet.js
@@ -0,0 +1,11 @@
+function responsesToSheet(e){
+  var form = FormApp.getActiveForm();
+  var currentItemResponses = e.response.getItemResponses();
+  answer = [[e.response.getTimestamp()]];
+  for (var i = 0; i < currentItemResponses.length; i++) {    
+      answer[0].push(currentItemResponses[i].getResponse());
+  };
+  Logger.log(answer)
+  const ss = SpreadsheetApp.openById("your-spreadsheet-id");
+  ss.getSheetByName('your-sheet-name').getRange(ss.getLastRow()+1,1,1,answer[0].length).setValues(answer);
+}
